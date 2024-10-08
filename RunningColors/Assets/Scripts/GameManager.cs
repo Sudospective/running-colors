80746e9b830c6234e533206cf70f8d72e28e7712
Sudospective <avery@sudospective.net>
2024-10-08 13:36:24 -0700

    Create personal scene, GameManager

---
+++ RunningColors/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    static private GameManager instance;
+    public GameObject player;
+    public Camera mainCamera;
+
+    public GameManager GetInstance() { return instance; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
