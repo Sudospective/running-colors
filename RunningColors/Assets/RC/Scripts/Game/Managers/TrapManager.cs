8cff3c1c366a85cb8d8292838e7e362896f38714
TheTinKan <trentanm71@gmail.com>
2024-10-10 17:14:04 -0400

    Completed fan trap, tagged the player, and added a collider to the player.

---
+++ RunningColors/Assets/RC/Scripts/Game/Managers/TrapManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapManager : MonoBehaviour
+{
+    public GameObject fanTrap;
+    private ParticleSystem windEffect;
+    private AudioSource fanAudio;
+    
+    [SerializeField] float pushForce = 10f;
+    [SerializeField] float activeTime = 2f;
+    [SerializeField] float inactiveTime = 3f;
+    
+
+    private bool fanIsOn;
+    private float timer = 0f;
+    private Vector3 pushDir;
+
+    private Animator fanAnimator;
+
+    void Start()
+    {
+        pushDir = -fanTrap.transform.forward;
+        windEffect = fanTrap.GetComponentInChildren<ParticleSystem>();
+        fanAudio = fanTrap.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (fanIsOn)
+        {
+            if(timer>=activeTime)
+            {
+                fanIsOn = false;
+                timer = 0f;
+                windEffect.Stop();
+                fanAudio.Stop();
+            }
+        }
+        else
+        {
+            if(timer>=inactiveTime)
+            {
+                fanIsOn = true;
+                timer = 0f;
+                windEffect.Play();
+                fanAudio.Play();
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(fanIsOn && other.CompareTag("Player"))
+        {
+            Debug.Log("Fan is on and player detected.");
+
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+            if(playerRb != null)
+            {
+                Debug.Log("Applying force to player.");
+
+                playerRb.AddForce(pushDir * pushForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
