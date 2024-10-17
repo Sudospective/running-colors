937fca4d684acc4fdda98d2154560d5db9db4090
Sudospective <avery@sudospective.net>
2024-10-17 15:36:35 -0700

    Update PlayerState.cs

--- RunningColors/Assets/RC/Scripts/Game/Singletons/PlayerState.cs
+++ RunningColors/Assets/RC/Scripts/Game/Singletons/PlayerState.cs
@@ -7,10 +7,23 @@ public class PlayerState : MonoBehaviour
     static private PlayerState instance;
     static public PlayerState GetInstance() { return instance; }
 
+    [Header("Paint")]
+    public int paintCurrentAmount;
+    public int paintMaxAmount;
+    public PaintType paintCurrentType;
+
     // Start is called before the first frame update
     void Awake()
     {
-        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
