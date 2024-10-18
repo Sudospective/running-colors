33f7b8a73f8463bdc43c6fc1ca417717e9bb299c
Samira Barber <156492611+SamiraB-dev@users.noreply.github.com>
2024-10-18 11:43:17 -0500

    Removed scriptable events

--- RunningColors/Assets/RC/Scripts/Game/SceneLoading/ScriptableObjects/PersistantGameplaySO.cs
+++
@@ -1,3 +0,0 @@
-//CreateAssetMenu commented since we don't want to create more than one PersistantGameplay GameSceneSO
-//[CreateAssetMenu(fileName = "PersistantGameplay", menuName = "Scene Data/PersistantGameplay")]
-public class PersistantGameplaySO : GameSceneSO { }
