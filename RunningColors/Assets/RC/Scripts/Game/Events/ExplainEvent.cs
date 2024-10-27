e80beda3413fab90bdebd3a814329036f40d1c64
Sudospective <avery@sudospective.net>
2024-10-27 14:33:22 -0700

    Fix ExplainEvent.cs

--- RunningColors/Assets/RC/Scripts/Game/Events/ExplainEvent.cs
+++ RunningColors/Assets/RC/Scripts/Game/Events/ExplainEvent.cs
@@ -9,6 +9,7 @@ public class ExplainEvent : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.CompareTag("Player")) return;
         if (notifObject != null)
         {
             notifPublisher.RaiseEvent(notifObject);
